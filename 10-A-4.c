8bb52ee178bc9870270981391622f09e13200ce9
MONANK23 <140997714+MONANK23@users.noreply.github.com>
2023-10-20 11:14:07 +0530

    Add files via upload

---
+++ 10-A-4.c
@@ -0,0 +1,14 @@
+//Print digits of given number.
+#include<stdio.h>
+void main() {
+	int n,i,rem;
+	printf("Enter Number :");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		rem=n%10;
+		printf("%d\n",rem);
+		n=n/10;
+	}
+}
+
